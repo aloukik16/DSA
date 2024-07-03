cf0aae7174b761bbe6caf10f142a3829c05508f6
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-07-03 17:02:20 +0530

    2.Opearots Updated

---
+++ 2.Operators/QuesThree.java
@@ -0,0 +1,11 @@
+package Operators;
+public class QuesThree {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = y = z = 2;
+        x += y;
+        y -= z;
+        z /= (x + y);
+        System.out.println(x + " " + y + " " + z);
+    }
+}
