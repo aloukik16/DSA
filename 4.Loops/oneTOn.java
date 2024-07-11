0087f9fa07324dd07ecd929c3917177dff48e976
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-07-11 11:51:38 +0530

    4.Loops Updated

---
+++ 4.Loops/oneTOn.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class oneTOn {
+    public static void main(String[] args) {
+        int counter = 1;
+        int n = 7;
+        while (counter <= n) {
+            System.out.print(counter+ " ");
+            counter++;
+        }
+        System.out.println("Printed 1 to n using While Loop.");
+    }
+}
---
+++ 4.Loops/oneTOten.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class oneTOten {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.print(counter+ " ");
+            counter++;
+        }
+        System.out.println("Printed 1 to 10 using While Loop.");
+    }
+}
